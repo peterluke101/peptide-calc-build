7a6630919bdb3dd376313a6daa74d2afa55a221c
Pete <peterluke101@gmail.com>
2026-04-14 11:54:30 -0700

    chore: bump service worker cache to v27
    Invalidates v26 cache on next load to pick up all Phase 1 bug fixes.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'peptide-calc-v26';
+const CACHE_NAME = 'peptide-calc-v27';
 const ASSETS = ['./', './index.html', './schedule.html', './manifest.json', './logs-journal.js'];
 
 self.addEventListener('install', e => {
